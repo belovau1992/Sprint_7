5b8277f1a2febe7ab90112dff8a475774fe85d78
Andrew Below <belovau1992@yandex.com>
2025-05-27 23:16:53 +0300

    create urls.py

---
+++ urls.py
@@ -0,0 +1,5 @@
+BASE_URL = 'https://qa-scooter.praktikum-services.ru'
+CREATE_COURIER_URL = '/api/v1/courier'
+CREATE_ORDER_URL = '/api/v1/orders'
+LOGIN_URL = '/api/v1/courier/login'
+GET_ORDER_BY_TRACK = '/api/v1/orders/track'  # + track number(например ?t=123456)
